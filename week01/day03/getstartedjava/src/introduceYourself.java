25db80fedf36e2e49ada09217ad99524be6eb506
Octavio <nboctavio@gmail.com>
2018-02-15 15:21:56 +0100

    random save

---
+++ week01/day03/getstartedjava/src/introduceYourself.java
@@ -0,0 +1,24 @@
+public class introduceYourself {
+    public static void main(String[] args) {
+        // Write a program that prints a few details to the terminal window about you
+        // It should print each detail to a new line.
+        //  - Your name
+        //  - Your age
+        //  - Your height in meters (as a decimal fraction)
+        //
+        //  Example output:
+        //  John Doe
+        //  31
+        //  1.87
+
+
+
+
+
+        System.out.println("John doe");
+        System.out.println(31);
+        System.out.println(1.83);
+    }
+}
+
+white ciklus invalid command
