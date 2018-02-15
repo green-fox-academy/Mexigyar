2ac39005649165cb36915d97b1bc35271cb6182a
Octavio <nboctavio@gmail.com>
2018-02-15 16:49:34 +0100

    and another random save

---
+++ week01/day03/getstartedjava/src/defineBasicInfo.java
@@ -0,0 +1,23 @@
+public class defineBasicInfo {
+
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        // Your age as an integer
+        // Your height in meters as a double
+        // Whether you are married or not as a boolean
+
+        String myName = "Octavio";
+        int myAge = 20;
+        double myHeight = 1.83;
+        boolean married = false;
+
+
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(myHeight);
+        System.out.println(married);
+
+    }
+
+}
--- week01/day03/getstartedjava/src/favoriteNumber.java
+++ week01/day03/getstartedjava/src/favoriteNumber.java
@@ -6,7 +6,7 @@ public class favoriteNumber {
         // Store your favorite number in a variable (as a number)
         // And print it like this: "My favorite number is: 8"
 
-        System.out.println("my favorite number is" + favorite_number);
+        System.out.println("my favorite number is " + favorite_number);
 
     }
 
