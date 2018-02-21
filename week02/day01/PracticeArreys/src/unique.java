94cfe6d93c001633757660123854f22ae75a9ec0
Octavio <nboctavio@gmail.com>
2018-02-21 20:39:44 +0100

    k

--- week02/day01/PracticeArreys/src/unique.java
+++ week02/day01/PracticeArreys/src/unique.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class unique {
     public static void main(String[] args) {
         //  Create a function that takes a list of numbers as a parameter
@@ -5,7 +10,31 @@ public class unique {
 
         //  Example
         System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
-        //  should print: `[1, 11, 34, 52, 61]`
+          //should print: `[1, 11, 34, 52, 61]`
+
+        //List<Integer> mzArraz = new ArrayList<>(Arrays.asList(2, 3, 4, 5, 4, 4, 9, 2));
+        //mzArraz = mzArraz.stream()
+            //    .distinct()
+          //      .collect(Collectors.toList());
+        //System.out.println(mzArraz);
+
     }
 
-    public static int[] unique()
+    public static int[] unique(int [] inputArray) {
+        Arrays.sort(inputArray);
+
+
+        int[] outputArrey = {};
+        for (int i = 0; i < inputArray.length; i++) {
+
+            if (inputArray[i] != inputArray[i + 1]) {
+                System.out.println(inputArray[i]);
+//                outputArrey.add(inputArray[i]);
+            }
+
+
+        }
+
+        return outputArrey ;
+    }
+}
