60ddd9e0bf8384e792a605ab3088422baf7083bb
Octavio <nboctavio@gmail.com>
2018-02-28 16:10:31 +0100

    Complex Architectures

---
+++ week03/day03/src/Teacher.java
@@ -0,0 +1,15 @@
+public class Teacher {
+    /*
+    Create Student and Teacher classes
+Student
+learn()
+question(teacher) -> calls the teachers answer method
+Teacher
+teach(student) -> calls the students learn method
+answer()
+     */
+
+    
+
+}
+
