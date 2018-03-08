a7f758f24ffd6e19f94dd57082ad3cdd9a02b47b
Octavio <nboctavio@gmail.com>
2018-03-08 16:47:53 +0100

    Sum Game

---
+++ week04/day04/TestFolder/SumTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SumTest {
+
+    @Test
+    public void fill() {
+    }
+
+    @Test
+    public void printList() {
+    }
+
+    @Test
+    public void summList() {
+    }
+
+    @Test
+    public void intScanner() {
+    }
+}
--- week04/day04/src/Sum.java
+++ week04/day04/src/Sum.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 public class Sum {
     ArrayList<Integer> integerList = new ArrayList<>();
 
+
+    //constructor
+    public Sum() {
+        this.integerList = new ArrayList<Integer>();
+    }
+
+    //fills list in object
     public void fill(int numberOfElementsToAdd) {
         int randNumber;
         Random random = new Random();
@@ -14,15 +21,21 @@ public class Sum {
         }
     }
 
+    //empty list
+    public void emptyList(){
+        int rounds = integerList.size();
+        for (int i = 0; i < rounds; i++) {
+            integerList.remove(0);
+        }
+    }
+
+
     public void printList(){
         System.out.println(integerList.toString());
     }
 
-    public Sum() {
 
-        this.integerList = new ArrayList<Integer>();
-    }
-
+    //sums numbers in the objects list
     public int summList(){
         int sum= 0;
         for (int i = 0; i < integerList.size(); i++) {
@@ -31,7 +44,8 @@ public class Sum {
         return sum;
     }
 
-    public int intScanner(){
+    //scans in an int number
+    public static int intScanner(){
         int number = 0;
         try{
             Scanner sc;
@@ -44,17 +58,53 @@ public class Sum {
         return number;
     }
 
-    //method return boolean for checking if input:int variable equals summ()
+    //playOneRound
+    public static int PlayOneRound(Sum sum){
+        System.out.println("Sum the given numbers and enter your anwser");
+        sum.printList();
+        int answer = intScanner();
+
+        if (isItCorrectBoolean(sum, answer)){
+            System.out.println("TRUEEE");
+        } else
+        System.out.println("your answer is " + isItCorrectdifference(sum, answer) + " number(s) away from the correct one");//rewrite with better english
+        System.out.println("the Correct answer is " + isItCorrectdifference(sum, answer));
+        return sum.summList();
+    }
+
     //method input level x(how many numbers you have to add together) calls summ() and boolean method
+    public int levelPlay(int lvl, Sum sum){
+        int missTipping = 0;
+        sum.fill(lvl);
+        missTipping += PlayOneRound(sum);
+        sum.emptyList();
+        return missTipping ;
+
+    }
+
     //print at end of boolean method if anwser was correct or incorrect and how much was the difference.
-    //make game lvl 1-5 and return resul
-    //add points feature
+    public static boolean isItCorrectBoolean(Sum sum, int awnser){
+        return sum.summList() == awnser;
+    }
+
+    //return difference in absolute value
+    public static int isItCorrectdifference(Sum sum, int awnser){
+
+        return Math.abs(sum.summList() - awnser);
+    }
+
+    public int PlayGame(int rounds){
+        int missTipping = 0;
+        
+    }
+    //make game method level(int) is the input lvl 1-5 and return result
+    //add points feature later
+
+
 
     public static void main(String[] args) {
         Sum summingStuff = new Sum();
-        summingStuff.fill(4);
-        summingStuff.printList();
-
+        summingStuff.levelPlay(2,summingStuff);
     }
 
 
