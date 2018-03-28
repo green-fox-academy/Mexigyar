12c0f16b70d206d1e8af9fc3d0119d0dacfc71fa
Octavio <nboctavio@gmail.com>
2018-03-28 12:22:49 +0200

    save

--- week06/day03/src/Main.java
+++ week06/day03/src/Main.java
@@ -2,14 +2,7 @@ public class Main {
     public static void main(String[] args) {
         TodoList todoList = new TodoList();
         todoList.importList();
-        System.out.println("");
-        System.out.println("here are the arguments you gave");
-        for (int i = 0; i < args.length; i++) {
-            int plusOne = i + 1;
-            System.out.print("your " + plusOne + ". argument is ");
-            System.out.println("|" + args[i] + "|");
-        }
-        argHandler(args, todoList);
+        argHandler.argHandler(args, todoList);
         todoList.saveToFile();
     }
 
--- week06/day03/src/argHandler.java
+++ week06/day03/src/argHandler.java
@@ -1,5 +1,14 @@
 public class argHandler {
     public static void argHandler(String[] args, TodoList todoList){
+
+        System.out.println("");
+        System.out.println("here are the arguments you gave");
+        for (int i = 0; i < args.length; i++) {
+            int plusOne = i + 1;
+            System.out.print("your " + plusOne + ". argument is ");
+            System.out.println("|" + args[i] + "|");
+        }
+
         if (args.length == 0) {
             todoList.noArg();
         } else {
