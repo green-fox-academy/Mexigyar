0476ecdad3b9a7628e56070bb600fc3f424f7061
Octavio <nboctavio@gmail.com>
2018-04-03 16:35:48 +0200

    greet all

--- week07/day01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week07/day01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.lang.reflect.Array;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
     AtomicLong atomicLong = new AtomicLong(0);
+    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
 
     @RequestMapping("/web/greeting")
     public String greeting(Model model) {
@@ -16,5 +21,19 @@ public class HelloWebController {
         model.addAttribute("count", atomicLong.addAndGet(1));
 
         return "greeting";
+    }
+
+    @RequestMapping("/web/greetingtoall")
+    public String greetingAll(Model model) {
+        model.addAttribute("greet", iterateArray(hellos));
+        return "greetingAll";
+    }
+
+    public String iterateArray(String[] array) {
+        String hello = "";
+        for (int i = 0; i < array.length; i++) {
+            hello += " : " + array[i] + " : ";
+        }
+        return hello;
     }
 }
