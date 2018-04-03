448c400a96dc86e293789cbc17f9ecaf443ce5a2
Octavio <nboctavio@gmail.com>
2018-04-03 15:46:30 +0200

    web greet counter app without working counter

--- week07/day01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week07/day01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -8,7 +8,6 @@ import java.util.concurrent.atomic.AtomicLong;
 
 
 @RestController
-
 public class HelloRESTController {
     AtomicLong atomicLong = new AtomicLong(1);
 
--- week07/day01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week07/day01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -1,4 +1,20 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
 public class HelloWebController {
+
+    @RequestMapping("/web/greeting")
+    public String greeting(Model model) {
+        AtomicLong atomicLong = new AtomicLong(0);
+        model.addAttribute("name", " World");
+        model.addAttribute("count", atomicLong.addAndGet(1));
+
+        return "greeting";
+    }
 }
