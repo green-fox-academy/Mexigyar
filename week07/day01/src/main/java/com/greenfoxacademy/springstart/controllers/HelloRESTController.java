8b82e7f5842a8e68a843d73331c3dc1f72326b23
Octavio <nboctavio@gmail.com>
2018-04-03 15:18:03 +0200

    greeteing with atomiclong

--- week07/day01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week07/day01/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -3,16 +3,19 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.yaml.snakeyaml.events.Event;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 
 @RestController
 
 public class HelloRESTController {
+    AtomicLong atomicLong = new AtomicLong(1);
 
     @RequestMapping(value = "/greeting")
     public Greeting greeting(@RequestParam("name")String content){
-        Greeting greeting = new Greeting(1,"hello " + content);
+        Greeting greeting = new Greeting(atomicLong.addAndGet(1),"hello " + content);
         return greeting;
     }
+
 }
