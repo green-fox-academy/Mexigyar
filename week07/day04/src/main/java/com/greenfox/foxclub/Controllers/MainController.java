3e6444783ee69cf46eedde17b6d54294ea9aea8e
Octavio <nboctavio@gmail.com>
2018-04-06 13:28:39 +0200

    nutrition shop

--- week07/day04/src/main/java/com/greenfox/foxclub/Controllers/MainController.java
+++ week07/day04/src/main/java/com/greenfox/foxclub/Controllers/MainController.java
@@ -14,27 +14,41 @@ public class MainController {
 
 
     @GetMapping(value = "/")
-    public String index2(@RequestParam(name = "foxname", required = false) String name, Model model){
-        model.addAttribute("foxname",name);
-        model.addAttribute("food",fox.getFood());
-        model.addAttribute("drink",fox.getDrink());
+    public String index2(@RequestParam(name = "foxname", required = false) String name, Model model) {
+        model.addAttribute("foxname", name);
+        model.addAttribute("food", fox.getFood());
+        model.addAttribute("drink", fox.getDrink());
         return "index";
     }
 
+    @GetMapping(value = "/")
+    public String index2(@RequestParam(name = "food", required = false) String food, @RequestParam(name = "drink", required = false) String drink, Model model) {
+        model.addAttribute("foxname", fox.getName());
+        model.addAttribute("food", food);
+        model.addAttribute("drink", drink);
+        return "index";
+    }
+
+
     @GetMapping(value = "/login")
-    public String login(){
-        return  "login";
+    public String login() {
+        return "login";
     }
 
     @PostMapping(value = "/login")
-    public String login2(@ModelAttribute(name = "foxname") String foxname){
+    public String login2(@ModelAttribute(name = "foxname") String foxname) {
         fox.setName(foxname);
         return "redirect:/?foxname=" + foxname;
     }
 
 
     @GetMapping(value = "/nutrition_store")
-    public String nutStore(){
+    public String nutStore() {
         return "nutritionstore";
+    }
+
+    @PostMapping()
+    public String nutStore2(@ModelAttribute(name = "food") String food,@ModelAttribute(name = "drink") String drink) {
+        return "redirect:/?food=" + food + "&" + "?drink=" + drink;
     }
 }
