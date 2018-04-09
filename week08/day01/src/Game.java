05b29eebea91609e0958a4efa09698052410104c
Octavio <nboctavio@gmail.com>
2018-04-09 13:54:10 +0200

    save

--- week08/day01/src/Game.java
+++ week08/day01/src/Game.java
@@ -15,4 +15,15 @@ public class Game {
             return player2;
 
     }
+    /*
+    public boolean hasPair(Player player){
+        for (int i = 0; i < player.hand.size(); i++) {
+            player.hand.get(i).getValue()==
+
+        }
+
+
+        return true;
+    }
+    */
 }
--- week08/day01/test/GameTest.java
+++ week08/day01/test/GameTest.java
@@ -3,21 +3,44 @@ import org.junit.Test;
 
 public class GameTest {
     Game game = new Game();
-    Player Black = new Player();
-    Player White = new Player();
+    Player black = new Player();
+    Player white = new Player();
 
 
-   @Test
-    public void highCardTest(){
-        Black.newHand(new Card(2,"H"),new Card(3,"D"),new Card(5,"S"),new Card(9,"C"),new Card("K","D"));
-        White.newHand(new Card(2,"C"),new Card(3,"H"),new Card(4,"S"),new Card(8,"C"),new Card("A","H"));
+    @Test
+    public void highCardTest() {
+        black.newHand(
+                new Card(2, "H"),
+                new Card(3, "D"),
+                new Card(5, "S"),
+                new Card(9, "C"),
+                new Card("K", "D"));
+        white.newHand(
+                new Card(2, "C"),
+                new Card(3, "H"),
+                new Card(4, "S"),
+                new Card(8, "C"),
+                new Card("A", "H"));
 
-        Assert.assertEquals(White,game.highCard(Black,White));
+        Assert.assertEquals(white, game.highCard(black, white));
     }
+@Test
+public void highCardTest2() {
+        black.newHand(
+                new Card(5, "H"),
+                new Card(6, "D"),
+                new Card(8, "S"),
+                new Card(10, "C"),
+                new Card("A", "D"));
+        white.newHand(
+                new Card(5, "C"),
+                new Card(6, "H"),
+                new Card(7, "S"),
+                new Card(2, "C"),
+                new Card("J", "H"));
 
-
-
-
+        Assert.assertEquals(black, game.highCard(black, white));
+    }
 
 
 }
