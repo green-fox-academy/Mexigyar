56a568cde01b091db18847adc09fa30c69721e79
Octavio <nboctavio@gmail.com>
2018-04-10 15:24:23 +0200

    save

--- week08/day02b/src/main/java/com/greenfox/connectionwithmysql/ConnectionwithmysqlApplication.java
+++ week08/day02b/src/main/java/com/greenfox/connectionwithmysql/ConnectionwithmysqlApplication.java
@@ -1,5 +1,8 @@
 package com.greenfox.connectionwithmysql;
 
+import com.greenfox.connectionwithmysql.Model.Todo;
+import com.greenfox.connectionwithmysql.Repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +10,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConnectionwithmysqlApplication implements CommandLineRunner{
 
+	@Autowired
+	TodoRepository todoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConnectionwithmysqlApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		todoRepository.save(new Todo("make pancake"));
+		todoRepository.save(new Todo("give the pancake away"));
 
 	}
 }
--- week08/day02b/src/main/java/com/greenfox/connectionwithmysql/Model/Todo.java
+++ week08/day02b/src/main/java/com/greenfox/connectionwithmysql/Model/Todo.java
@@ -46,4 +46,20 @@ public class Todo {
         return id +
                 ". " + title + '\'';
     }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setUrgent(boolean urgent) {
+        this.urgent = urgent;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
 }
