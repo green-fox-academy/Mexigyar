1c0d5250de971515699392768a1fbe20c98f18e2
Octavio <nboctavio@gmail.com>
2018-04-16 12:26:21 +0200

    added controller

--- week09/day01/src/main/java/com/greefox/restdemo/Controllers/WebContoller.java
+++ week09/day01/src/main/java/com/greefox/restdemo/Controllers/WebContoller.java
@@ -1,4 +1,13 @@
 package com.greefox.restdemo.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class WebContoller {
+
+    @RequestMapping(value = "/")
+    public String index(){
+        return "index";
+    }
 }
