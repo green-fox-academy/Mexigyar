0f90f5346d8bc65dd34b030ad91a03e40c9d2fc8
Octavio <nboctavio@gmail.com>
2018-04-17 12:03:53 +0200

    starting project

---
+++ week09/demoexam/src/main/java/com/greefox/demoexam/DemoexamApplication.java
@@ -0,0 +1,12 @@
+package com.greefox.demoexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoexamApplication.class, args);
+	}
+}
