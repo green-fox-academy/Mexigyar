275c356c9c3b370435f4afd04a72ff97546a0126
Octavio <nboctavio@gmail.com>
2018-04-25 10:40:22 +0200

    Listing items logic

--- week10/src/main/java/com/greenfox/clothing/clothing/service/WarehouseService.java
+++ week10/src/main/java/com/greenfox/clothing/clothing/service/WarehouseService.java
@@ -1,8 +1,14 @@
 package com.greenfox.clothing.clothing.service;
 
+import com.greenfox.clothing.clothing.models.Warehouse;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface WarehouseService {
+
+    public List<String> getAvailableItems();
+
 
 }
--- week10/src/main/java/com/greenfox/clothing/clothing/service/WarehouseServiceImpl.java
+++ week10/src/main/java/com/greenfox/clothing/clothing/service/WarehouseServiceImpl.java
@@ -1,7 +1,38 @@
 package com.greenfox.clothing.clothing.service;
 
+import com.greenfox.clothing.clothing.models.Warehouse;
+import com.greenfox.clothing.clothing.repo.WarehouseRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.thymeleaf.expression.Lists;
 
-public class WarehouseServiceImpl {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class WarehouseServiceImpl implements WarehouseService {
+
+    @Autowired
+    WarehouseRepo warehouseRepo;
+
+    @Override
+    public List<String> getAvailableItems() {
+        List<Warehouse> listWithDuplicates = new ArrayList<>();
+        warehouseRepo.findAll().forEach(listWithDuplicates::add);
+        List<String> itemNames = toItemNameList(listWithDuplicates)
+                ;
+        List<String> listWithoutDuplicates = itemNames.stream()
+                .distinct()
+                .collect(Collectors.toList());
+        return listWithoutDuplicates;
+    }
+    
+    List<String> toItemNameList(List<Warehouse> warehouseList) {
+
+        List<String> stringList = new ArrayList<String>();
+
+        for (Warehouse warehouse : warehouseList) {
+            stringList.add(warehouse.getItemName());
+        }
+        return stringList;
+    }
 }
